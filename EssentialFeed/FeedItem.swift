6bd21289eaf45a826202514a6260a36218dadbe9
Alex Tapia <alejandrovztapia@gmail.com>
2020-12-19 16:21:16 -0600

    Created the FeedItem module

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alex Tapia on 19/12/20.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
