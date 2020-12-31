514658bf2dd8885de90bd146476c7f179a784f66
Alex Tapia <alejandrovztapia@gmail.com>
2020-12-30 21:46:35 -0600

    Simplify error type declarations with a helper function.

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
 
-    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+    expect(sut, toCompleteWith: failure(.connectivity), when: {
       let clientError = NSError(domain: "Test", code: 0)
       client.complete(with: clientError)
     })
@@ -49,7 +49,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     let samples = [199, 201, 300, 400, 500]
     samples.enumerated().forEach { index, code in
-      expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+      expect(sut, toCompleteWith: failure(.invalidData), when: {
         let json = makeItemsJSON([])
         client.complete(withStatusCode: code, data: json, at: index)
       })
@@ -59,7 +59,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
     let (sut, client) = makeSUT()
     
-    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+    expect(sut, toCompleteWith: failure(.invalidData), when: {
       let invalidJSON = Data("invalid json".utf8)
       client.complete(withStatusCode: 200, data: invalidJSON)
     })
@@ -120,6 +120,10 @@ extension RemoteFeedLoaderTests {
     trackForMemoryLeaks(sut, file: file, line: line)
     trackForMemoryLeaks(client, file: file, line: line)
     return (sut, client)
+  }
+  
+  private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+    return .failure(error)
   }
   
   private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
