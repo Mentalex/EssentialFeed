2353c6bd30113ad7074514696d22868c5210669c
Alex Tapia <alejandrovztapia@gmail.com>
2020-12-30 21:01:20 -0600

    Remove `LoadFeedResult` Equatable conformance from production since it's test-only requirement

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -12,9 +12,8 @@ public enum LoadFeedResult<Error: Swift.Error> {
   case failure(Error)
 }
 
-extension LoadFeedResult: Equatable where Error: Equatable {}
-
 protocol FeedLoader {
+  
   associatedtype Error: Swift.Error
   
   func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
