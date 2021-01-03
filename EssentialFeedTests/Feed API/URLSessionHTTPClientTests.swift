7d5573fc63bf46e30ced826bbcc4b308a85cdcba
Alex Tapia <alejandrovztapia@gmail.com>
2021-01-02 23:17:02 -0600

    Resumes data task on creation.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,7 +15,7 @@ class URLSessionHTTPClient {
   }
   
   func get(from url: URL) {
-    session.dataTask(with: url) { _, _, _ in }
+    session.dataTask(with: url) { _, _, _ in }.resume()
   }
 }
 
@@ -31,16 +31,48 @@ class URLSessionHTTPClientTests: XCTestCase {
     XCTAssertEqual(session.receivedURLs, [url])
   }
   
+  func test_getFromURL_resumesDataTaskWithURL() {
+    let url = URL(string: "http://any-url.com")!
+    let session = URLSessionSpy()
+    let task = URLSessionDataTaskSpy()
+    
+    // NOTE: We need to tell the `session` to return our `task`
+    // for a given url. So we created a Stubbing Mechanism:
+    session.stub(url: url, task: task)
+    
+    let sut = URLSessionHTTPClient(session: session)
+    
+    sut.get(from: url)
+    
+    XCTAssertEqual(task.resumeCallCount, 1)
+  }
+  
   // MARK: - Helpers
   
   private class URLSessionSpy: URLSession {
     var receivedURLs = [URL]()
+    private var stubs = [URL: URLSessionDataTask]()
+    
+    func stub(url: URL, task: URLSessionDataTask) {
+      stubs[url] = task
+    }
     
     override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
       receivedURLs.append(url)
-      return FakeURLSessionDataTask()
+      return stubs[url] ?? FakeURLSessionDataTask()
     }
   }
   
-  private class FakeURLSessionDataTask: URLSessionDataTask {}
+  private class FakeURLSessionDataTask: URLSessionDataTask {
+    // NOTE: We need to override `resume` method to avoid crash.
+    override func resume() {}
+  }
+  
+  private class URLSessionDataTaskSpy: URLSessionDataTask {
+    var resumeCallCount = 0
+    
+    override func resume() {
+      resumeCallCount += 1
+    }
+  }
 }
