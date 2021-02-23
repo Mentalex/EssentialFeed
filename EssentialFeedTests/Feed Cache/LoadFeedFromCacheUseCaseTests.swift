796accd9b1acbefd74c492a8789d0c30b39cadb1
Alex Tapia <alejandrovztapia@gmail.com>
2021-02-23 07:37:58 -0600

    Load command deletes seven days old cache upon retrieval.

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -103,6 +103,18 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     XCTAssertEqual(store.receivedMessages, [.retrieve])
   }
+  
+  func test_load_deletesCacheOnSevenDaysOldCache() {
+    let feed = uniqueImageFeed()
+    let fixedCurrentDate = Date()
+    let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+    let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+    
+    sut.load { _ in }
+    store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+  }
 }
 
 // MARK: - Helpers
