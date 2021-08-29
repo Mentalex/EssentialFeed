96d8f0e65cee417254efccf041067b54c2d01db6
Alex Tapia <alejandrovztapia@gmail.com>
2021-08-29 16:42:23 -0500

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,33 @@
+//
+//  FeedCacheTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Alex Tapia on 29/08/21.
+//
+
+import Foundation
+import EssentialFeed
+
+func uniqueImage() -> FeedImage {
+  return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+  let models = [uniqueImage(), uniqueImage()]
+  let local = models.map {
+    LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
+  }
+  return (models, local)
+}
+
+
+extension Date {
+  
+  func adding(days: Int) -> Date {
+    return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+  }
+  
+  func adding(seconds: TimeInterval) -> Date {
+    return self + seconds
+  }
+}
