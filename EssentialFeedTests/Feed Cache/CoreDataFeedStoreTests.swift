4ed2db67fed4356cee28455ddfb82d87fc5aea90
Alex Tapia <alejandrovztapia@gmail.com>
2021-09-26 21:47:43 -0500

    Load persisten container stores upon `CoreDataFeedStore` initialization

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -63,7 +63,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
   // MARK: - Helpers
   
   func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
-    let sut = CoreDataFeedStore()
+    let storeBundle = Bundle(for: CoreDataFeedStore.self)
+    let sut = try! CoreDataFeedStore(bundle: storeBundle)
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
   }
