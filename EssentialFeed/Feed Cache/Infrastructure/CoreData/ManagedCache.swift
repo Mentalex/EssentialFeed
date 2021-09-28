bb41d9eae4fe83925248bfcf57e45e01cd353595
Alex Tapia <alejandrovztapia@gmail.com>
2021-09-27 21:47:33 -0500

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,31 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Alex Tapia on 27/09/21.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+  
+  @NSManaged var timestamp: Date
+  @NSManaged var feed: NSOrderedSet
+  
+  static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+    let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+    request.returnsObjectsAsFaults = false
+    return try context.fetch(request).first
+  }
+  
+  static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+    try find(in: context).map(context.delete)
+    return ManagedCache(context: context)
+  }
+  
+  var localFeed: [LocalFeedImage] {
+    return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+  }
+}
+
---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Alex Tapia on 27/09/21.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+  
+  @NSManaged var id: UUID
+  @NSManaged var imageDescription: String?
+  @NSManaged var location: String?
+  @NSManaged var url: URL
+  @NSManaged var cache: ManagedCache
+  
+  static func image(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    return NSOrderedSet(array: localFeed.map { local in
+      let managed = ManagedFeedImage(context: context)
+      managed.id = local.id
+      managed.imageDescription = local.description
+      managed.location = local.location
+      managed.url = local.url
+      return managed
+    })
+  }
+  
+  var local: LocalFeedImage {
+    return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+  }
+}
