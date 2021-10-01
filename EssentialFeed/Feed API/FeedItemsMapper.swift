c2de2c1c90a61770298b397bd841bbe001da5517
Alex Tapia <alejandrovztapia@gmail.com>
2021-09-30 22:38:56 -0500

    Remove redundant internal access control declarations

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-internal final class FeedItemsMapper {
+ final class FeedItemsMapper {
   
   private struct Root: Decodable {
     let items: [RemoteFeedItem]
@@ -15,7 +15,7 @@ internal final class FeedItemsMapper {
   
   private static var OK_200: Int { return 200 }
   
-  internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+   static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
     guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
       throw RemoteFeedLoader.Error.invalidData
     }
