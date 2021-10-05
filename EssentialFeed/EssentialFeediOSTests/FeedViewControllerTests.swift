f14de2ea6ce73c5cdab4b5492f6944a3a5d64c11
Alex Tapia <alejandrovztapia@gmail.com>
2021-10-05 14:15:16 -0500

    Decouple tests from specific UI controls for user initiated reloads with a test-specific DSL method

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -50,14 +50,14 @@ final class FeedViewControllerTests: XCTestCase {
     XCTAssertEqual(loader.loadCallCount, 1)
   }
   
-  func test_pullToRefresh_loadsFeed() {
+  func test_userInitiatedFeedReload_loadsFeed() {
     let (sut, loader) = makeSUT()
     sut.loadViewIfNeeded()
     
-    sut.refreshControl?.simulatePullToRefresh()
+    sut.simulateUserInitiatedFeedReload()
     XCTAssertEqual(loader.loadCallCount, 2)
     
-    sut.refreshControl?.simulatePullToRefresh()
+    sut.simulateUserInitiatedFeedReload()
     XCTAssertEqual(loader.loadCallCount, 3)
   }
   
@@ -78,18 +78,18 @@ final class FeedViewControllerTests: XCTestCase {
     XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
   }
   
-  func test_pullToRefresh_showsLoadingIndicator() {
+  func test_userInitiatedFeedReload_showsLoadingIndicator() {
     let (sut, _) = makeSUT()
     
-    sut.refreshControl?.simulatePullToRefresh()
+    sut.simulateUserInitiatedFeedReload()
     
     XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
   }
   
-  func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion() {
+  func test_userInitiatedFeedReload_hidesLoadingIndicatorOnLoaderCompletion() {
     let (sut, loader) = makeSUT()
     
-    sut.refreshControl?.simulatePullToRefresh()
+    sut.simulateUserInitiatedFeedReload()
     loader.completFeedLoading()
     
     XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
@@ -119,6 +119,13 @@ final class FeedViewControllerTests: XCTestCase {
     func completFeedLoading() {
       completions[0](.success([]))
     }
+  }
+}
+
+private extension FeedViewController {
+
+  func simulateUserInitiatedFeedReload() {
+    refreshControl?.simulatePullToRefresh()
   }
 }
 
