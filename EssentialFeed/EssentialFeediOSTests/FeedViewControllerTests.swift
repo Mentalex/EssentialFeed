d2386f74e43c38799fb420c8736216cf79ef2133
Alex Tapia <alejandrovztapia@gmail.com>
2021-10-08 22:55:41 -0500

    Hide loading indicator on both load error and success

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -32,13 +32,13 @@ final class FeedViewControllerTests: XCTestCase {
     XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once view is loaded")
 
     loader.completFeedLoading(at: 0)
-    XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading is completed")
+    XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading completes successfully")
 
     sut.simulateUserInitiatedFeedReload()
     XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once user initiates a reload")
 
-    loader.completFeedLoading(at: 1)
-    XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading is completed")
+    loader.completFeedLoadingWithError(at: 1)
+    XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading completes with error")
   }
   
   func test_loadFeedCompletion_rendersSuccessfullyLoadedFed() {
