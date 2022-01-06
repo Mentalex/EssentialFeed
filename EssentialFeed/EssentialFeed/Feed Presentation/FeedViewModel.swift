ea21a12416d8732628a8af4392c4b1fbf3939c22
Alex Tapia <alejandrovztapia@gmail.com>
2022-01-05 22:36:01 -0600

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Alex Tapia on 05/01/22.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+  public let feed: [FeedImage]
+}
