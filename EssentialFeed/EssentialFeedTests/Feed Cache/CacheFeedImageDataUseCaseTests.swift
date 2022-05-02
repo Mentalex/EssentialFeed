9a8f2d98c5caebcd5ec62276340ce47bfd08fa03
Alex Tapia <alejandrovztapia@gmail.com>
2022-05-01 21:16:59 -0700

    `LocalFeedImageDataLoader.saveImageDataFromURL` succeeds on successful store insertion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -35,6 +35,14 @@ class CacheFeedImageDataUseCaseTests: XCTestCase {
     })
   }
   
+  func test_saveImageDataFromURL_succeedsOnSuccessfulStoreInsertion() {
+    let (sut, store) = makeSUT()
+    
+    expect(sut, toCompleteWith: .success(()), when: {
+      store.completeInsertionSuccessfully()
+    })
+  }
+  
   // MARK: - Helpers
   
   private func makeSUT(currentDate: @escaping () -> Date = Date.init,
