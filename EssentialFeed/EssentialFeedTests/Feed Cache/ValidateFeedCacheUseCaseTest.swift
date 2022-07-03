e36abc602fa1aa4c5836a34366ad258588b6de00
Alex Tapia <alejandrovztapia@gmail.com>
2022-07-03 16:47:34 -0700

    `LocalFeedLoader.validateCache` fails on deletion error of a failed retrieval

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
@@ -70,6 +70,25 @@ class ValidateFeedCacheUseCaseTest: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
   }
   
+  func test_validateCache_failsOnDeletionErrorOfFailedRetrieval() {
+    let (sut, store) = makeSUT()
+    let deletionError = anyNSError()
+    
+    expect(sut, toCompleteWith: .failure(deletionError), when: {
+      store.completeRetrieval(with: anyNSError())
+      store.completeDeletion(with: deletionError)
+    })
+  }
+  
+  func test_validateCache_succeedsOnSuccessfulDeletionOfFailedRetrieval() {
+    let (sut, store) = makeSUT()
+    
+    expect(sut, toCompleteWith: .success(()), when: {
+      store.completeRetrieval(with: anyNSError())
+      store.completeDeletionSuccessfully()
+    })
+  }
+  
   func test_validateCache_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
     let store = FeedStoreSpy()
     var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
@@ -92,5 +111,31 @@ class ValidateFeedCacheUseCaseTest: XCTestCase {
     trackForMemoryLeaks(store, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, store)
+  }
+  
+  private func expect(_ sut: LocalFeedLoader,
+                      toCompleteWith expectedResult: LocalFeedLoader.ValidationResult,
+                      when action: () -> Void,
+                      file: StaticString = #filePath,
+                      line: UInt = #line) {
+    let exp = expectation(description: "Wait for load completion")
+    
+    sut.validateCache { receivedResult in
+      switch (receivedResult, expectedResult) {
+      case (.success, .success):
+        break
+        
+      case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+        XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+        
+      default:
+        XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+      }
+      
+      exp.fulfill()
+    }
+    
+    action()
+    wait(for: [exp], timeout: 1.0)
   }
 }
