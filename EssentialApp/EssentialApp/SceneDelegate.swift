77613c80a15a2697b7540399eb9503a247773752
Alex Tapia <alejandrovztapia@gmail.com>
2023-04-11 21:10:35 -0600

    Display remote feed on launch 	when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,6 +15,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
+    
+    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+    
+    let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    let remotefeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+    let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+    
+    window?.rootViewController = FeedUIComposer.feedComposedWith(
+      feedLoader: remotefeedLoader,
+      imageLoader: remoteImageLoader)
   }
 
 }
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,4 +8,13 @@
 import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
+  
+  func test_onLaunch_displayRemoteFeedWhenCustomerHasConnectivity() {
+    let app = XCUIApplication()
+    
+    app.launch()
+    
+    XCTAssertEqual(app.cells.count, 22)
+    XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+  }
 }
