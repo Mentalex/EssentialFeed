396e579f2a7394417319edbf46f9c4bbb1602d90
Alex Tapia <alejandrovztapia@gmail.com>
2023-06-25 16:44:17 -0600

    Remove unused debugging code that was previously used during UI tests.

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -13,16 +13,15 @@ import EssentialFeediOS
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
-  let localStoreURL = NSPersistentContainer
-    .defaultDirectoryURL()
-    .appendingPathComponent("feed-store.sqlite")
-  
   private lazy var httpClient: HTTPClient = {
     URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
   }()
   
   private lazy var store: FeedStore & FeedImageDataStore = {
-    try! CoreDataFeedStore(storeURL: localStoreURL)
+    try! CoreDataFeedStore(
+      storeURL: NSPersistentContainer
+        .defaultDirectoryURL()
+        .appendingPathComponent("feed-store.sqlite"))
   }()
   
   convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
@@ -40,9 +39,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func configureWindow() {
     let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
     
-    let remoteClient = makeRemoteClient()
-    let remotefeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
-    let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+    let remotefeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+    let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
     
     let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
     let localImageDataLoader = LocalFeedImageDataLoader(store: store)
@@ -60,9 +58,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             decoratee: remoteImageLoader,
             cache: localImageDataLoader)))
     )
-  }
-  
-  func makeRemoteClient() -> HTTPClient {
-    return httpClient
   }
 }
