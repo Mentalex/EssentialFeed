e50970fa309f120922c0e961c5911184131b4278
Alex Tapia <alejandrovztapia@gmail.com>
2025-10-09 11:51:20 -0600

    Fix Feed load issue

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -29,6 +29,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     LocalFeedLoader(store: store, currentDate: Date.init)
   }()
   
+  private lazy var remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+  
+  private lazy var remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+  
   convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
     self.init()
     self.httpClient = httpClient
@@ -56,10 +60,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-
-    let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-    
     return remoteFeedLoader
       .loadPublisher()
       .caching(to: localFeedLoader)
