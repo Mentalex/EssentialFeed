8efd3d23d8b7cd45856995b69a02a108ec6fd350
Alex Tapia <alejandrovztapia@gmail.com>
2026-02-07 11:09:51 -0600

    Replace RemoteLoader with HTTPClient publisher composed with FeedItemsMapper

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -31,8 +31,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   private lazy var remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
   
-  private lazy var remoteFeedLoader = RemoteLoader(url: remoteURL, client: httpClient, mapper: FeedItemsMapper.map)
-  
   convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
     self.init()
     self.httpClient = httpClient
@@ -60,8 +58,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-    return remoteFeedLoader
-      .loadPublisher()
+    return httpClient
+      .getPublisher(url: remoteURL)
+      .tryMap(FeedItemsMapper.map)
       .caching(to: localFeedLoader)
       .fallback(to: localFeedLoader.loadPublisher)
   }
